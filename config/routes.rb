20079715925365b55b94cc0c67568cd6b0a5b7c9
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-24 09:09:00 -0600

    adds route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :salaries, only: [:index]
     end
   end
 end
